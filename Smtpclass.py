1c81d8d95362a5e7678cbe2116f314cbc5b55194
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-08-22 13:33:08 -0300

    Add files via upload
    Atualizei o smtclass para evitar possíveis erros

--- Smtpclass.py
+++ Smtpclass.py
@@ -5,26 +5,35 @@ import smtplib
 import os
 
 
-mensagem = MIMEMultipart()
-mensagem['De'] = "seuemail@gmail.com"
-senha = "suasenha"
+
+senha = "113311gm"
 caminho = os.listdir('./Email')
+
 for cam in caminho:
-    img = f'./Email/{cam}'
+    try:
+        mensagem = MIMEMultipart()
+        mensagem['De'] = "guilhermemaia201450@gmail.com"
+        img = f'./Email/{cam}'
     
-    fp = open(img, 'rb')
-    mensagem.attach(MIMEImage(fp.read()))
-    fp.close()
+        fp = open(img, 'rb')
+        mensagem.attach(MIMEImage(fp.read()))
+        fp.close()
     
-    para = cam[:-4]
-    mensagem['Para'] = f"{para}.com"
+        para = cam[:-4]
+        mensagem['Para'] = f"{para}.com"
+    
+        servidor = smtplib.SMTP('smtp.gmail.com: 587')
+    
+        servidor.starttls()
 
-    servidor = smtplib.SMTP('smtp.gmail.com: 587')
-    
-    servidor.starttls()
+        servidor.login(mensagem['De'], senha)
 
-    servidor.login(mensagem['De'], senha)
+        servidor.sendmail(mensagem['De'], mensagem['Para'], mensagem.as_string())
 
-    servidor.sendmail(mensagem['De'], mensagem['Para'], mensagem.as_string())
-    
-    servidor.quit()
+        
+        servidor.quit()
+        del mensagem
+        print(f"{para}.com enviado")
+    except:
+        print(f"Email invalido: {para}.com")
+        del mensagem
